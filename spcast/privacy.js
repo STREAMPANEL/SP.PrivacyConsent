34bd0552f365bb4c5cb471178b81cb22d850d658
scysys <scysys@outlook.com>
2023-05-30 14:35:29 +0200

    Wrong Domain

--- spcast/privacy.js
+++ spcast/privacy.js
@@ -70,7 +70,7 @@ function setCookie(name, value, days) {
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = "; expires=" + date.toUTCString();
   }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=.streampanel.net";
+  document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=.spcast.eu";
 }
 
 // Function to get a cookie
